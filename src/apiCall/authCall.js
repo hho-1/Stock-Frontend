0ff4e67ffca7b9de31d9ecb72d9aa220d39a6acc
Habip Hakkioglu <h.hakkioglu@icloud.com>
2023-09-16 17:40:26 +0200

    useAuthCall custom hook created

---
+++ src/apiCall/authCall.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { fetchFail, fetchStart, registerSuccess } from "../features/authSlice";
+import axios from "axios";
+
+
+
+const useAuthCall = () => {                     //custom Hook 'use' ile baslamak zorunda
+  const dispatch = useDispatch();               //Bunu direkt olarak kullanamiyorsun, mecburen bir custom hook ile sarmalliyoruz. useAuthCall bir custom hook
+
+  const register = async userInfo => {
+    dispatch(fetchStart());
+    try {
+      const { data } = await axios.post(
+        "http://16105.fullstack.clarusway.com/account/register/",
+        userInfo
+      );
+      console.log(data);
+      dispatch(registerSuccess(data));
+    } catch (error) {
+      dispatch(fetchFail());
+    }
+  };
+  return register;
+};
+
+export default useAuthCall;
