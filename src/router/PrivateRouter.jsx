a5c3f428f91b82789b9a3492168c0f8a2f0dfb0b
Habip Hakkioglu <h.hakkioglu@icloud.com>
2024-07-04 14:49:44 +0200

    url

--- src/router/PrivateRouter.jsx
+++ src/router/PrivateRouter.jsx
@@ -4,7 +4,7 @@ import { Navigate, Outlet } from "react-router-dom";
 const PrivateRouter = () => {
   const { currentUser } = useSelector(state => state.auth);
 
-  return currentUser ? <Outlet /> : <Navigate to="/" />;
+  return currentUser ? <Outlet /> : <Navigate to="/stock" />;
 };
 
 export default PrivateRouter;
