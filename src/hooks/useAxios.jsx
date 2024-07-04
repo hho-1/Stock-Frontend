96c062318da7dc5e95ffb35005de5ff515d02be8
Habip Hakkioglu <h.hakkioglu@icloud.com>
2024-07-04 14:43:22 +0200

    url change

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -6,7 +6,7 @@ const useAxios = () => {
     const {token} = useSelector(state => state.auth)
   
     const axiosWithToken = axios.create({                      //Bu bir instance'dir, Baska instance'lar da olusturulabilir.
-        baseURL: process.env.REACT_APP_BASE_URL,
+        baseURL: process.env.BASE_URL,
         headers: {Authorization: `Token ${token}`}
     });
 
